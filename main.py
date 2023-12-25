8a70d62f2f6f5f773cd1bc39a94ea4676a73a6d0
josh140520 <143544549+josh140520@users.noreply.github.com>
2023-12-26 05:15:45 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -127,7 +127,7 @@ class MainWindow(Screen): #Main screen
 
     ######################################
     Mainfontsize = NumericProperty(sp(20)) ##editable Main window font size
-    PagePicSize = NumericProperty(dp(55))
+    PagePicSize = NumericProperty(dp(200))
     #FontLayoutsize = NumericProperty(sp(20), sp(20))
 
 
