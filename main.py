6f378641966104bef89c22b96f4b555341591579
josh140520 <143544549+josh140520@users.noreply.github.com>
2024-01-05 12:31:15 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1878,7 +1878,7 @@ class GraphWindow(Screen): #3rd window
 
             # Calculate line coordinates based on listX and listY
             listX = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
-            listY = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+            listY = [None, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, None]
             print(f"the y values: {listY} and lenY {len(listY)} and lenX {len(listX)}")
 
 
