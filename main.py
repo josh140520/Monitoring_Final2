454eab4ecac5b2d0d7d7e7bc37830d9519acded8
josh140520 <143544549+josh140520@users.noreply.github.com>
2024-01-18 16:31:30 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -3300,7 +3300,7 @@ class GraphWindow(Screen): #3rd window
 
             # Calculate line coordinates based on listX and listY
             listX = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
-            listY = list(batt_active.values())
+            listY = [None, 2, None, 4, None, 6, None, 8, None, 10, None, 12, None, 14, None, 16, None, 18, None, 20, None, 22, None, 24]
             print(f"the y values: {listY} and lenY {len(listY)} and lenX {len(listX)}")
 
 
@@ -3413,7 +3413,7 @@ class GraphWindow(Screen): #3rd window
                 if drawY[0] is None:
                     drawY[0] = 0
                     self.draw_rectangle3(0)
-                for i in range(0, 23):
+                for i in range(1, 24):
                     if drawY[i] is None:
                         self.draw_rectangle3(i)
             except:
@@ -3424,12 +3424,12 @@ class GraphWindow(Screen): #3rd window
             # Bind line points to update dynamically when the layout size changes
 
     def draw_rectangle3(self, listypos):
-        x1 = 0.045
+        x1 = 0.043
         x2 = 0.0352
         resize_factor = x1  # per hour 0.037
         height_factor = 0.7
         heightposition_factor = 0.84
-        position_factor = 0.84 - (2.185 * (listypos * x2))  # hour position
+        position_factor = 0.84 - (2.202 * (listypos * x2))  # hour position
 
         Color(0, 0, 0, 1)  # Set color to blue with alpha (RGB values + alpha)
         self.rectangle = Rectangle(
